ec561f719d9c39b5836790af9d4c615cfd4fd8e5
shoapp <125400285+shoapp@users.noreply.github.com>
2024-02-15 14:27:35 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -86,15 +86,19 @@ const Post: React.FC<PostProps> = (props) => {
 				
 				
 				
-				<meta property="og:site_name" content={host.split('.')[0]} />
-				
-				
+				<meta property="og:title" content={post.title} />
+				<meta property="og:description" content={removeTags(post.excerpt)} />
+				<meta property="og:type" content="article" />
+				<meta property="og:locale" content="en_US" />
+				<meta property="og:site_name" content={host.split('FACEBOOK.COM')[0]} />
+				<meta property="article:published_time" content={post.dateGmt} />
+				<meta property="article:modified_time" content={post.modifiedGmt} />
 				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
 				<meta
 					property="og:image:alt"
 					content={post.featuredImage.node.altText || post.title}
 				/>
-				
+				<title>{post.title}</title>
 				
 			</Head>
 			<div className="post-container">
