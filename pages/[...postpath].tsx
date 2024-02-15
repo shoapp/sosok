3803274d5da54c1f391d6c1691065528d1c083db
shoapp <125400285+shoapp@users.noreply.github.com>
2024-02-15 14:22:40 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -84,13 +84,11 @@ const Post: React.FC<PostProps> = (props) => {
 		<>
 			<Head>
 				
-				<meta property="og:title" content={post.title} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
+				
+				
 				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
+				
+				
 				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
 				<meta
 					property="og:image:alt"
@@ -100,7 +98,7 @@ const Post: React.FC<PostProps> = (props) => {
 				
 			</Head>
 			<div className="post-container">
-				<h1>{post.title}</h1>
+				
 				<img
 					src={post.featuredImage.node.sourceUrl}
 					alt={post.featuredImage.node.altText || post.title}
